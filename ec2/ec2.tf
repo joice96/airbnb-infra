0b7409e8fad87ff99354236d871f16f821b1dda9
joice96 <107009989+joice96@users.noreply.github.com>
2024-06-29 10:50:16 -0400

    Update ec2.tf
    fix change

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-01107263728f3bef4"
   instance_type = "t2.micro"
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   vpc_security_group_ids = [var.sg_id]
 
   tags = {
